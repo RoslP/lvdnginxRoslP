60f37659232e88a391f8f0290c0dbf164a1a5e3f
pavel99 <ght12345ght12345@gmail.com>
2024-11-08 09:12:19 +1000

    beginer pain

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {return 'this is home page';});
+Route::get('/home2', 'App\Http\Controllers\MyFirstController@index123');

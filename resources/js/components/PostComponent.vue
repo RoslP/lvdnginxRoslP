e96a8d3cb5b793bfc2c78653b4238ca33e53c8ec
pavel99 <ght12345ght12345@gmail.com>
2024-12-04 08:51:42 +1000

    methods and computed

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -13,6 +13,19 @@ export default {
       boolean: false,
       someNull: null
     }
+  },
+  methods: {
+    sayHello() {
+      console.log('Hello')
+    },
+    sayHi() {
+      console.log('Hi')
+    }
+  },
+  computed: {
+    computedMethod() {
+      return 'my name is Oleg'
+    }
   }
 }
 
@@ -21,9 +34,12 @@ export default {
 <template>
   <div><p>This is PostComponent </p>
     <SingePostComponent></SingePostComponent>
-    name : {{name}}
-    age : {{age}}
+    name : {{ name }}
+    age : {{ age }}
+    computed : {{computedMethod}}
   </div>
+  <button type="button" @click="this.sayHello()">Hello</button>
+  <button type="button" @click="this.sayHi()">Hi</button>
 </template>
 
 <style scoped>

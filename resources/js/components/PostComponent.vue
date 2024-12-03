0af179baa7a461ebdc44e97b657543edda1c7bd6
pavel99 <ght12345ght12345@gmail.com>
2024-12-04 08:05:54 +1000

    Options api Components + @vue/language-server@2.0.28

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,21 @@
+<script>
+import SingePostComponent from "./SingePostComponent.vue";
+
+export default {
+  name: "PostComponent",
+  components: {
+    SingePostComponent
+  }
+}
+
+</script>
+
+<template>
+  <div><p>This is PostComponent </p>
+    <SingePostComponent></SingePostComponent>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

872aac9f5077413f64ff1c4df80b8362e28f8090
pavel99 <ght12345ght12345@gmail.com>
2024-12-06 02:52:41 +1000

    ref, $ref, $parent, props:[], :

---
+++ resources/js/components/SomeComponent.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: "SomeComponent",
+    components: {},
+    methods: {},
+    mounted() {
+        this.$parent.$parent.parentLog2()
+    },
+    props:[
+       'obj'
+    ]
+
+}
+</script>
+
+<template>
+    <div>
+        <div>Color: {{obj.color}}</div>
+        <div>Number: {{obj.number > 10 ? 'more 10' : 'less 10'}}</div>
+        <div>Is published: {{ obj.isPublished ? 'Published' : 'Not a Published'}}</div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>

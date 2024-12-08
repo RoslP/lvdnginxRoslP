02401cdc5668271f370e3240e49e7573593ae767
pavel99 <ght12345ght12345@gmail.com>
2024-12-09 08:46:49 +1000

    breeze

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\Post\IndexController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostsController;
-use App\Http\Controllers\AboutController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,36 +11,28 @@ use App\Http\Controllers\AboutController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/',[\App\Http\Controllers\HomeController::class,'index'])->name('home');
-
-Route::group(['namespace' => 'App\Http\Controllers\Post'], function () {
-    Route::get('/posts', IndexController::class)->name('post.index');
-    Route::get('/posts/create', CreateController::class)->name('post.create');
-    Route::post('/posts', StoreController::class)->name('post.store');
-    Route::get('/posts/{post}', ShowController::class)->name('post.show');
-    Route::get('/posts/{post}/edit', EditController::class)->name('post.edit');
-    Route::patch('/posts/{post}', UpdateController::class)->name('post.update');
-    Route::delete('/posts/{post}', DestroyController::class)->name('post.delete');
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::group(['namespace' => '\App\Http\Controllers\Admin\Post','middleware'=>'admin'], function () {
-    Route::prefix('/admin')->group(function () {
-        Route::get('/post', 'IndexController')->name('admin.post.index');
-    });
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/main', 'App\Http\Controllers\MainController@index')->name('main.index');
-
-
-
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/{page}',\App\Http\Controllers\IndexController1::class)->where('page', '.*');
-
+require __DIR__.'/auth.php';

3035b6279444fe1a00ea8eda36492ecfdc04c7db
pavel99 <ght12345ght12345@gmail.com>
2024-12-09 21:17:45 +1000

    index message component vue (without websockets)

---
+++ resources/js/Pages/Messages/MessagesIndex.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "MessagesIndex"
+}
+</script>
+
+<template>
+    <div class="w-1/2 mx-auto">
+        <div class="pt-8 mb-4">
+            <h1 class="text-lg mb-8">Messages</h1>
+        </div>
+        <div class="mb-4">
+            <input class="rounded-full border border-gray-400" type="text" placeholder="your message">
+        </div>
+        <div class="mb-4">
+            <button class="block rounded-lg w-48 border-2 border-sky-600 bg-sky-600 text-white text-center py-2 hover:bg-sky-200 hover:text-sky-600">Send</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>

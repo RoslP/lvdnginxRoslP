fb16020a3c1eb863a2c9c65619ac182f231d6ac6
pavel99 <ght12345ght12345@gmail.com>
2024-12-14 22:30:21 +1000

    websocket dynamic public channels

--- app/Http/Controllers/User/StoreController.php
+++ app/Http/Controllers/User/StoreController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Events\DynamicSendEvent;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\StoreRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function __invoke( StoreRequest $request)
+    public function __invoke(User $user, StoreRequest $request)
     {
-        $data = $request->validated();
+        $data =$request->validated();
+        broadcast(new DynamicSendEvent($user->id,"this is uid ${data['from_id']}"))->toOthers();
+        return response()->json($data);
     }
 }
--- app/Http/Requests/User/StoreRequest.php
+++ app/Http/Requests/User/StoreRequest.php
@@ -22,7 +22,7 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'body'=>'string|required',
+            'from_id'=>'required|integer|exists:users,id',
         ];
     }
 }
